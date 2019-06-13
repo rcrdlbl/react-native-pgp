f19315c0110f604ee6dd4afd46168b29a2cbd082
rcrdlbl <bluh@outlook.com>
2019-06-12 20:52:59 -0400

    Settings Screen Replaced

---
+++ components/SettingsView.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { View, Text, Button } from 'react-native'
+
+class SettingsView extends Component {
+  render() {
+    return(
+      <View>
+        <Text>Settings Screen</Text>
+      </View>
+    )
+  }
+}
+
+export default SettingsView
